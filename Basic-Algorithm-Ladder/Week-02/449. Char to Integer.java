a4ef3ab12381070fe0473ab5ad7fcdd08bc7b493
Liyu Qin <qinliyu1990@gmail.com>
2016-08-01 14:47:25 -0700

    Create 449. Char to Integer.java

---
+++ Basic-Algorithm-Ladder/Week-02/449. Char to Integer.java
@@ -0,0 +1,16 @@
+public class Solution {
+    /**
+     * @param character a character
+     * @return an integer
+     */
+    public int charToInteger(char character) {
+        // Write your code here
+        return (int) character;
+    }
+}
+
+
+/*
+	用 (int) 来将 char 转化为 integer.
+	注意这里不能将 String 转化为 integer.....详见本周第 54 题。
+*/
