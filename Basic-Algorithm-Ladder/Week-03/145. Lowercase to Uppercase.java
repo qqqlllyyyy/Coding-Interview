32ca0722e494104fb702c1315566474a1fe8c7d1
Liyu Qin <qinliyu1990@gmail.com>
2016-08-03 20:14:25 -0700

    Create 145. Lowercase to Uppercase.java

---
+++ Basic-Algorithm-Ladder/Week-03/145. Lowercase to Uppercase.java
@@ -0,0 +1,10 @@
+public class Solution {
+    /**
+     * @param character a character
+     * @return a character
+     */
+    public char lowercaseToUppercase(char character) {
+        // Write your code here
+        return Character.toUpperCase(character);
+    }
+}
