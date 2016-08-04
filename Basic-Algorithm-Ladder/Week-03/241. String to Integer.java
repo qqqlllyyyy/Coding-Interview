d58f58d1fbcc41551b177828fbbc23f71991caf0
Liyu Qin <qinliyu1990@gmail.com>
2016-08-03 20:16:15 -0700

    Create 241. String to Integer.java

---
+++ Basic-Algorithm-Ladder/Week-03/241. String to Integer.java
@@ -0,0 +1,15 @@
+public class Solution {
+    /**
+     * @param str a string
+     * @return an integer
+     */
+    public int stringToInteger(String str) {
+        // Write your code here
+        return Integer.parseInt(str);
+    }
+}
+
+/*
+	这里貌似用 valueOf 或者 parseInt 都能通过
+	更多例子见 http://www.mkyong.com/java/java-convert-string-to-int/
+*/
