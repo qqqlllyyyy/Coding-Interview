3bd7f26406e8745fa8e75172e9a7f3fdbbbc0e69
Liyu Qin <qinliyu1990@gmail.com>
2016-08-03 20:15:06 -0700

    Create 147. Narcissistic Number.java

---
+++ Basic-Algorithm-Ladder/Week-03/147. Narcissistic Number.java
@@ -0,0 +1,29 @@
+class Solution {
+    /*
+     * @param n: The number of digits. 
+     * @return: All narcissistic numbers with n digits.
+     */
+    public ArrayList<Integer> getNarcissisticNumbers(int n) {
+        // write your code here
+        
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        
+        if (n == 1) {
+            for (int i = 0; i <= 9; i++){
+                result.add(i);
+            }
+            return result;
+        }
+        
+        for (int i = (int) Math.pow(10, n - 1); i < (int) Math.pow(10, n); i++){
+            int j = i;
+            int s = 0;
+            while(j > 0){
+                s += (int) Math.pow(j % 10, n);
+                j /= 10;
+            }
+            if (s == i) result.add(i);
+        }
+        return result;
+    }
+};
