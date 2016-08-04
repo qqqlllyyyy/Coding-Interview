ffe349fe8f58b7c81724368a30efcaf40f791511
Liyu Qin <qinliyu1990@gmail.com>
2016-08-04 15:14:36 -0700

    Create 454. Rectangle Area.java

---
+++ Basic-Algorithm-Ladder/Week-03/454. Rectangle Area.java
@@ -0,0 +1,25 @@
+public class Rectangle {
+    /*
+     * Define two public attributes width and height of type int.
+     */
+    // write your code here
+    public int width, height;
+    /*
+     * Define a constructor which expects two parameters width and height here.
+     */
+    // write your code here
+    public Rectangle (int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+    
+    
+    /*
+     * Define a public method `getArea` which can calculate the area of the
+     * rectangle and return.
+     */
+    // write your code here
+    public int getArea() {
+        return this.width * this.height;
+    }
+}
