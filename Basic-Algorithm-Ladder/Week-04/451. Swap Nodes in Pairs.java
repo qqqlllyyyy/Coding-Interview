4b56fa853aafd98d9ff0a0297034df096a94a18a
Liyu Qin <qinliyu1990@gmail.com>
2016-08-15 21:23:08 -0700

    Create 451. Swap Nodes in Pairs.java

---
+++ Basic-Algorithm-Ladder/Week-04/451. Swap Nodes in Pairs.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**
+     * @param head a ListNode
+     * @return a ListNode
+     */
+    public ListNode swapPairs(ListNode head) {
+        // Write your code here
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        
+        while (head.next != null && head.next.next != null) {
+            ListNode n1 = head.next;
+            ListNode n2 = head.next.next;
+            
+            // head->n1->n2->...
+            // => head->n2->n1->...
+            head.next = n2;
+            n1.next = n2.next;
+            n2.next = n1;
+            
+            // move to next pair
+            head = n1;
+        }
+        
+        return dummy.next;
+    }
+}
