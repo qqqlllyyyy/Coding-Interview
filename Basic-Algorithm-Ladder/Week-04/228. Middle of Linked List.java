c482e18da85c163c6eaf3a1eb82d46b54a250bba
Liyu Qin <qinliyu1990@gmail.com>
2016-08-15 21:21:57 -0700

    Create 228. Middle of Linked List.java

---
+++ Basic-Algorithm-Ladder/Week-04/228. Middle of Linked List.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param head: the head of linked list.
+     * @return: a middle node of the linked list
+     */
+    public ListNode middleNode(ListNode head) { 
+        // Write your code here
+        if (head == null) return null;
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
+
+/*
+    'fast != null && fast.next != null' 这个条件要注意，找几种情况试一试，比如 1->null, 1->2->null.
+*/
