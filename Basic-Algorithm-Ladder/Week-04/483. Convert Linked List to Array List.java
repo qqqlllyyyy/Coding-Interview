3b3673dd13fba576885b6790572ef44c647e0f5b
Liyu Qin <qinliyu1990@gmail.com>
2016-08-18 09:26:52 -0700

    Create 483. Convert Linked List to Array List.java

---
+++ Basic-Algorithm-Ladder/Week-04/483. Convert Linked List to Array List.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head the head of linked list.
+     * @return an array list
+     */
+    public List<Integer> toArrayList(ListNode head) {  
+        // Write your code here
+        List<Integer> result = new ArrayList<Integer>();
+        while (head != null){
+            result.add(head.val);
+            head = head.next;
+        }
+        return result;
+    }
+}
