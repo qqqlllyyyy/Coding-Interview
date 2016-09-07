713dc338ca5a34c6a1564fcba654cead1e10fc96
Liyu Qin <qinliyu1990@gmail.com>
2016-09-07 10:32:30 -0700

    Update 66. Binary Tree Preorder Traversal.java

--- Medium-Algorithm-Ladder/Week-03/66. Binary Tree Preorder Traversal.java
+++ Medium-Algorithm-Ladder/Week-03/66. Binary Tree Preorder Traversal.java
@@ -31,3 +31,36 @@ public class Solution {
     }
 }
 
+/*  20160907
+    优先使用下面的非递归方法并且要背过
+*/
+
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Preorder in ArrayList which contains node values.
+     */
+     
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if (root == null) {
+            return result;
+        }
+        
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        
+        return result;
+    }
+}
+
