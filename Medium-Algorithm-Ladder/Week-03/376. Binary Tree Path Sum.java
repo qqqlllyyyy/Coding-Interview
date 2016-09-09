665d3a761f4a322d172c7252efdc4e910879c6ec
Liyu Qin <qinliyu1990@gmail.com>
2016-09-09 11:09:27 -0700

    Update 376. Binary Tree Path Sum.java

--- Medium-Algorithm-Ladder/Week-03/376. Binary Tree Path Sum.java
+++ Medium-Algorithm-Ladder/Week-03/376. Binary Tree Path Sum.java
@@ -47,3 +47,47 @@ public class Solution {
     其实我这个解法有点不straight-forward，答案的解法如下，会在一开始先把root加入path中。
     两种方法都可以，想清楚就行。
 */
+
+/*  20160909
+    第二次做这个题就想一开始把root加入path中，解法如下:
+*/
+public class Solution {
+    /**
+     * @param root the root of binary tree
+     * @param target an integer
+     * @return all valid paths
+     */
+    public List<List<Integer>> binaryTreePathSum(TreeNode root, int target) {
+        // Write your code here
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        List<Integer> path = new ArrayList<Integer>();
+        path.add(root.val);
+        helper(result, root, path, root.val, target);
+        return result;
+    }
+    
+    private void helper(List<List<Integer>> result,
+                        TreeNode root, // Last node in path
+                        List<Integer> path,
+                        int currentSum,
+                        int target) {
+        if (currentSum == target && root.left == null && root.right == null) {
+            result.add(new ArrayList<Integer>(path));
+            return;
+        }
+        if (root.left != null) {
+            path.add(root.left.val);
+            helper(result, root.left, path, currentSum + root.left.val, target);
+            path.remove(path.size() - 1);
+        }
+        if (root.right != null) {
+            path.add(root.right.val);
+            helper(result, root.right, path, currentSum + root.right.val, target);
+            path.remove(path.size() - 1);
+        }
+        return;
+    }
+}
