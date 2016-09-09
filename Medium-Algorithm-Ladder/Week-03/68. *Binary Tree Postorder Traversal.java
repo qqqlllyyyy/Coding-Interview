1f203dd307a7faa5ed34af73e667825455e08fb6
Liyu Qin <qinliyu1990@gmail.com>
2016-09-09 16:07:14 -0700

    Update 68. *Binary Tree Postorder Traversal.java

--- Medium-Algorithm-Ladder/Week-03/68. *Binary Tree Postorder Traversal.java
+++ Medium-Algorithm-Ladder/Week-03/68. *Binary Tree Postorder Traversal.java
@@ -33,3 +33,43 @@ public class Solution {
 /*
     Recursive solution is trivial, do it iteratively.
 */
+
+/*  20160909
+    *不用递归的方法做，而用迭代，有点难理解，再写几遍。
+    与67题不同的是一开始创立stack的时候就要把root放进去。
+    遇到一个点之后，先把它的左节点放到stack中，只有当左节点不存在的时候才放右节点 (60-62行)
+    左右节点都存在的话，那放右节点入栈的操作是通过 63-66 行实现的。
+*/
+
+public ArrayList<Integer> postorderTraversal(TreeNode root) {
+    ArrayList<Integer> result = new ArrayList<Integer>();
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    TreeNode prev = null; // previously traversed node
+    TreeNode curr = root;
+
+    if (root == null) {
+        return result;
+    }
+
+    stack.push(root);
+    while (!stack.empty()) {
+        curr = stack.peek();
+        if (prev == null || prev.left == curr || prev.right == curr) { // traverse down the tree
+            if (curr.left != null) {
+                stack.push(curr.left);
+            } else if (curr.right != null) {
+                stack.push(curr.right);
+            }
+        } else if (curr.left == prev) { // traverse up the tree from the left
+            if (curr.right != null) {
+                stack.push(curr.right);
+            }
+        } else { // traverse up the tree from the right
+            result.add(curr.val);
+            stack.pop();
+        }
+        prev = curr;
+    }
+
+    return result;
+}
