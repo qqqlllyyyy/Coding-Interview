cfda05d65d4cd1a82769684eb0dc2fc06aae430d
Liyu Qin <qinliyu1990@gmail.com>
2016-09-09 10:12:37 -0700

    Create 111. Climbing Stairs.java

---
+++ Medium-Algorithm-Ladder/Week-04/111. Climbing Stairs.java
@@ -0,0 +1,20 @@
+public class Solution {
+    /**
+     * @param n: An integer
+     * @return: An integer
+     */
+    public int climbStairs(int n) {
+        // write your code here
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        result[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+        return result[n];
+    }
+}
