a3eb6725c51d733f9521cac27e2666ed1b46223c
Liyu Qin <qinliyu1990@gmail.com>
2016-09-09 15:53:50 -0700

    Update 67. *Binary Tree Inorder Traversal.java

--- Medium-Algorithm-Ladder/Week-03/67. *Binary Tree Inorder Traversal.java
+++ Medium-Algorithm-Ladder/Week-03/67. *Binary Tree Inorder Traversal.java
@@ -33,3 +33,37 @@ public class Solution {
 /*
     Recursive solution is trivial, do it iteratively.
 */
+
+/*  20160909
+    用非recursive的方法做，注意一开始创建 stack 之后不要就把 root 加进来。
+    因为要先找到最左边的点，用 current 来记录访问的点。
+*/
+
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        // write your code here
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode current = root;
+        
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            current = node.right;
+        }
+        return result;
+    }
+}
