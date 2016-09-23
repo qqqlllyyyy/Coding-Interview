57781cfbaa674126f6616887012a9d169ba28b93
Liyu Qin <qinliyu1990@gmail.com>
2016-09-23 08:46:18 -0700

    Create 166. Nth to Last Node in List.java

---
+++ Medium-Algorithm-Ladder/Week-06/166. Nth to Last Node in List.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @param n: An integer.
+     * @return: Nth to last node of a singly linked list. 
+     */
+    ListNode nthToLast(ListNode head, int n) {
+        // write your code here
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
+/*  20160922
+    这里 'Nth to Last Node' 的意思是倒数第N个节点。
+*/
