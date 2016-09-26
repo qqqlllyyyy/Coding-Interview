210a0aec22b4f9e146f6d6c461f59c6d68eecb66
Liyu Qin <qinliyu1990@gmail.com>
2016-09-25 22:00:22 -0700

    Create 35. Reverse Linked List.java

---
+++ Medium-Algorithm-Ladder/Week-06/35. Reverse Linked List.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The head of linked list.
+     * @return: The new head of reversed linked list.
+     */
+    public ListNode reverse(ListNode head) {
+        // write your code here
+        ListNode pre = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
+}
+
+/*  20160915
+    链表的基础操作，必须极其熟练.
+*/
